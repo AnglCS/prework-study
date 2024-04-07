ff1652c1425a594524c36dfc3b93bbcdf99b757d
An <angelcdsv@gmail.com>
2024-04-07 13:59:58 -0600

    added JavaScript

---
+++ prework-study-guide/assets/script.js
@@ -0,0 +1,33 @@
+const topics = ['HTML','CSS','Git','JavaScript'];
+
+for (let i= 0; i < topics.length; i++) {
+
+    console.log(topics[i]);
+}
+
+
+/* 
+const topic = "HTML";
+
+if (topic === 'HTML'){
+
+    console.log("Let's study HTML!");
+
+} else if (topic === 'CSS'){
+
+    console.log("Lets study CSS!");
+
+} else if (topic === 'Git'){
+
+    console.log("Let's study Git!");
+
+} else if (topic === 'JavaScript'){
+
+    console.log("Let´s study JavaScript");
+
+} else {
+
+    console.log('Please Try again');
+
+}
+*/
